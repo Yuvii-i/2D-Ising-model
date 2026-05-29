188980aa52b518baa711390c9525fa2d790f225e
Yuvraj Singh <yuvrajsingh11733@gmail.com>
2026-05-29 11:08:31 +0530

    Update finite size scaling

--- finite_size_scaling.py
+++ finite_size_scaling.py
@@ -1,108 +1,98 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import random
-import time
-start = time.time()
+from single_system import onesweep
 
-Nlist = [10,20,30,40]
-K = 1.0
-T = 1.0
-J = 1.0
-burnin = 300
-
-
-
-def onesweep(N,T,n):
+def simulation_ki(n,N,t,burnin,steps,ki=10,J=1,K=1):
     
+    """
+    Same structure as the 'simulation' function in 'single_system.py' but only measures
+    the magnetization and only every ki steps to reduce autocorrelation.
     
-    
-    deltaE_b1 = 2*J*n[::2,1::2] * (np.roll(n,1,axis=0)[::2,1::2] + np.roll(n,-1,axis=0)[::2,1::2] + np.roll(n,1,axis=1)[::2,1::2] + np.roll(n,-1,axis=1)[::2,1::2])
-    deltaE_b2 = 2*J*n[1::2,::2] * (np.roll(n,1,axis=0)[1::2,::2] + np.roll(n,-1,axis=0)[1::2,::2] + np.roll(n,1,axis=1)[1::2,::2] + np.roll(n,-1,axis=1)[1::2,::2])
-    
-    n[::2,1::2] *= -1
-    n[1::2,::2] *= -1
-    
-    b1 = n[::2,1::2].copy()
-    b2 = n[1::2,::2].copy()
-    
-    b1[np.random.random(deltaE_b1.shape) > np.exp(-deltaE_b1/(K*T))] *= -1
-    b2[np.random.random(deltaE_b2.shape) > np.exp(-deltaE_b2/(K*T))] *= -1
-    
-    n[::2,1::2] = b1 
-    n[1::2,::2] = b2
-    
-    deltaE_w1 = 2*J*n[::2,::2] * (np.roll(n,1,axis=0)[::2,::2] + np.roll(n,-1,axis=0)[::2,::2] + np.roll(n,1,axis=1)[::2,::2] + np.roll(n,-1,axis=1)[::2,::2])
-    deltaE_w2 = 2*J*n[1::2,1::2] * (np.roll(n,1,axis=0)[1::2,1::2] + np.roll(n,-1,axis=0)[1::2,1::2] + np.roll(n,1,axis=1)[1::2,1::2] + np.roll(n,-1,axis=1)[1::2,1::2])
-    
-    n[::2,::2] *= -1
-    n[1::2,1::2] *= -1
-    
-    w1 = n[::2,::2].copy()
-    w2 = n[1::2,1::2].copy()
-    
-    w1[np.random.random(deltaE_w1.shape) > np.exp(-deltaE_w1/(K*T))] *= -1
-    w2[np.random.random(deltaE_w2.shape) > np.exp(-deltaE_w2/(K*T))] *= -1
-    
-    n[::2,::2] = w1
-    n[1::2,1::2] = w2
+    Parameters:
+        ki: Number of steps after which the magnetization of the system is measured.
     
 
-for N in Nlist:
+    Returns:
+        np.mean(absmvalue): The average of the absolute magnetization value over all the steps.
+    """
+    
+    absmvalue = []
+        
+    s = 0
 
-    n = np.ones([N,N], dtype=int)
+    while s < burnin:
+            
+        s += 1
+            
+        onesweep(n,N,t,J,K)
+            
+    _ = 0
 
+    for k in range(steps):
+            
+        _ += 1
+            
+        onesweep(n,N,t,J,K)
+            
+        if _%ki == 0:
+            absmvalue.append(abs(np.sum(n)))
+    
+    return np.mean(absmvalue)
+
+def run_and_plot(Nlist, temp, burnin, steps):
+    
+    """
+    Parameters:
+        Nlist: A list of different grid sizes for different systems.
+        temp: An array of temperatures for the system to simulate the process.
+        burnin: The initial steps required for the system to stabalize are insignificant
+                for our measurements of observables so we skip them.
+        steps: The number of steps after the burnin steps.
+        
+    Simulates the process for different grid sizes over a range of temperatures, and
+    plots the Magnetization for every temperature for single grid and moves to next
+    grid size.   
+    
+    """
+    
+    for N in Nlist:
+
+        n = np.ones([N,N], dtype=int)
+
+        mavg = []
+
+        for t in temp:
+            
+            mag = simulation_ki(n,N,t,burnin,steps)
+            
+            mavg.append(mag)
+            
+        plt.plot(temp,np.array(mavg)/(N**2), label=N)
+    
+def show_plot():
+    
+    """
+    Adds labels and title to the plot and shows the plot.
+    """
+    
+    plt.axvline(2.2691853, label="Onsager's Tc value")
+    plt.xlabel('Temperature')
+    plt.ylabel('Magnetization')
+    plt.title('Temperature vs Magnetization')
+    plt.legend()    
+    plt.show()          
+
+def main():
+    Nlist = [10,20,30,40]
     temp = np.arange(1,4,0.01)
-
-    mavg = []
-    eavg = []
-    C = []
-    X = []
-
-
-    for t in temp:
-        
-        absmvalue = []
-        mvalue = []
-        evalue = []
-        
-        steps = 0
-
-        while steps < burnin:
-            
-            steps += 1
-            
-            onesweep(N,t,n)
-            
-        ki = 0
-
-        for k in range(8000):
-            
-            ki += 1
-            
-            onesweep(N,t,n)
-            
-            #E = -J*n*(np.roll(n,1,axis=0) + np.roll(n,1,axis=1))
-            #evalue.append(np.sum(E))
-            
-            if ki%10 == 0:
-                m = np.sum(n)
-                absmvalue.append(abs(m))
-                mvalue.append(m)
-        
-        #C.append(np.var(evalue)/((N**2)*(t**2)))
-        #X.append(np.var(mvalue)/((N**2)*t))
-        #eavg.append(np.mean(evalue))
-        mavg.append(np.mean(absmvalue))
-        
-    plt.plot(temp,np.array(mavg)/(N**2), label=N)
-
-plt.axvline(2.2691853)
-plt.xlabel('Temperature')
-plt.ylabel('Magnetization')
-plt.title('Temperature vs Magnetization')
-plt.legend()    
-plt.show()
-            
-print(time.time() - start)
-
+    K = 1.0
+    J = 1.0
+    burnin = 300
+    steps = 8000
     
+    run_and_plot(Nlist,temp,burnin,steps)
+    
+    show_plot()
+    
+if __name__ == "__main__":
+    main()
